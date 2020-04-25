0293ae3bd7743775446bdb8193ace26866efc2e3
XENK <xenkings@gmail.com>
2020-04-25 17:26:34 +0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+</script>
+<Tailwindcss />
+
+<main>
+	<!-- Full width column -->
+	<div class="flex mb-4">
+		<div class="flex-1 bg-gray-500 h-12"></div>
+	</div>
+
+	<!-- Five columns -->
+	<div class="flex mb-4">
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+	</div>
+
+	<!-- Seven columns -->
+	<div class="flex mb-4">
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+	</div>
+
+	<!-- Eleven columns -->
+	<div class="flex mb-4">
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+		<div class="flex-1 bg-gray-500 h-12"></div>
+		<div class="flex-1 bg-gray-400 h-12"></div>
+	</div>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'sans': 'Noto Sans, sans-serif'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
